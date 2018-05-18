6f5f06726d6929392d7dcf2b6293f42f3d79db1a
Marcel Lindig <ml@nullpilot.de>
2018-05-18 17:52:45 +0200

    batched uploads

--- src/uploadStream.js
+++ src/uploadStream.js
@@ -2,6 +2,9 @@ import { Writable } from 'readable-stream'
 import { sendToBrokers } from './utils/backend'
 
 const DEFAULT_OPTIONS = Object.freeze({
+    batchSize: 50,
+    maxParallelUploads: 2,
+    maxRetries: 10,
     objectMode: true
   })
 
@@ -15,7 +18,11 @@ export default class UploadStream extends Writable {
     this.sessIdA = sessIdA
     this.sessIdB = sessIdB
     this.chunkBuffer = []
+    this.batchBuffer = []
     this.chunkId = 0
+    this.ongoingUploads = 0
+    this.retries = 0
+    this.finishCallback = null
   }
   _write (data, encoding, callback) {
     const id = this.chunkId++
@@ -26,16 +33,83 @@ export default class UploadStream extends Writable {
       hash: this.genesisHash
     })
 
-    // TODO: Upload batches
-    // Currently not testable
+    if(this.chunkBuffer.length === this.options.batchSize) {
+      this.batchBuffer.push(this.chunkBuffer)
+      this.chunkBuffer = []
+      this._attemptUpload()
+    }
 
     callback()
   }
   _final (callback) {
-    sendToBrokers(this.sessIdA, this.sessIdB, this.chunkBuffer).then(() => {
+    this.finishCallback = callback
+
+    if(this.chunkBuffer.length > 0) {
+      this.batchBuffer.push(this.chunkBuffer)
+      this._attemptUpload()
+    } else if(this.ongoingUploads === 0) {
       callback()
+    }
+  }
+  _attemptUpload () {
+    if(this.ongoingUploads >= this.options.maxParallelUploads) {
+      return
+    }
+
+    const chunks = this.batchBuffer.shift()
+    this._upload(chunks)
+  }
+  _upload (chunks) {
+    this.ongoingUploads++
+
+    // Cork stream when busy
+    if(this.ongoingUploads === this.options.maxParallelUploads) {
+      this.cork()
+    }
+
+    const upload = sendToBrokers(this.sessIdA, this.sessIdB, chunks)
+
+    upload.then((result) => {
+      this._afterUpload()
     }).catch(error => {
-      callback(error)
+      this._uploadError(error, chunks)
     })
+  }
+  _afterUpload () {
+    this.ongoingUploads--
+
+    // Upload until done
+    if(this.batchBuffer.length > 0) {
+      return this._attemptUpload()
+    }
+
+    if(this.finishCallback) {
+      // Finish
+      if(this.ongoingUploads === 0) {
+        this.finishCallback()
+      }
+    } else {
+      // Continue
+      process.nextTick(() => this.uncork())
+    }
+  }
+  _uploadError (error, chunks) {
+    this.ongoingUploads--
+
+    console.warn('error', error)
+
+    if(this.retries++ < this.options.maxRetries) {
+      console.log('retrying', this.retries, 'of', this.options.maxRetries)
+      this.batchBuffer.push(chunks)
+      this._attemptUpload()
+      return
+    }
+
+    if(this.finishCallback) {
+      this.finishCallback(error)
+    } else {
+      this.emit('error', error)
+      this.close()
+    }
   }
 }
