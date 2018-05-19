02d855d08eac6ffe14c843fa8fa1e2b932e2b529
Marcel Lindig <ml@nullpilot.de>
2018-05-19 13:23:06 +0200

    add parallel downloads

--- src/downloadStream.js
+++ src/downloadStream.js
@@ -1,8 +1,10 @@
 import { Readable } from 'readable-stream'
-import * as Util from './util'
+import { queryGeneratedSignatures } from './utils/backend'
+import { offsetHash } from './util'
 
 const DEFAULT_OPTIONS = Object.freeze({
-    chunksPerBatch: 50,
+    maxParallelDownloads: 4,
+    chunksPerBatch: 500,
     // WIP. Must be passed for now
     iota: null,
     objectMode: false
@@ -19,19 +21,27 @@ export default class DownloadStream extends Readable {
     super(opts)
     this.options = opts
     this.numChunks = metadata.numberOfChunks
-    this.hash = Util.offsetHash(genesisHash, 0)
+    this.hash = offsetHash(genesisHash, 0)
     this.chunkOffset = 0
     this.chunkBuffer = []
-    this.isDownloading = false
     this.isDownloadFinished = false
+    this.processBatchId = 0
+    this.batchId = 0
+    this.batches = {}
     this.pushChunk = false
+    this.ongoingDownloads = 0
 
-    this._download()
+    if(this.numChunks === 0) {
+      this.push(null)
+    } else {
+      this._download()
+    }
   }
   _read () {
     this.pushChunk = true
 
-    if (!this.isDownloading && !this.isDownloadFinished) {
+    const attemptDownload = this.ongoingDownloads < this.options.maxParallelDownloads
+    if (!this.isDownloadFinished && attemptDownload) {
       this._download()
     }
 
@@ -42,11 +52,24 @@ export default class DownloadStream extends Readable {
       return
     }
 
+    if (this.chunkBuffer.length === 0) {
+      // Try for next batch
+      if(this.batches.hasOwnProperty(this.processBatchId)) {
+        this.chunkBuffer = this.batches[this.processBatchId]
+        delete this.batches[this.processBatchId]
+        this.processBatchId++
+      // Done, end stream
+      } else if (this.isDownloadFinished) {
+        this.push(null)
+      // Wait
+      } else {
+        return
+      }
+    }
+
     if (this.chunkBuffer.length) {
       this.pushChunk = this.push(this.chunkBuffer.shift())
       this._pushChunk()
-    } else if(this.isDownloadFinished) {
-      this.push(null)
     }
   }
   _download () {
@@ -56,38 +79,44 @@ export default class DownloadStream extends Readable {
                     this.options.chunksPerBatch)
 
     if(limit === 0) {
-      this.isDownloadFinished = true
-      this._pushChunk()
       return
     }
 
-    this.isDownloading = true
-    this.hash = Util.offsetHash(hash, limit - 1)
+    this.ongoingDownloads++
+    this.hash = offsetHash(hash, limit - 1)
     this.chunkOffset += limit
 
-
+    const batchId = this.batchId++
     const iota = this.options.iota
     const binaryMode = !this.options.objectMode
-    Util.queryGeneratedSignatures(iota, hash, limit, binaryMode).then(result => {
-      this.isDownloading = false
+    queryGeneratedSignatures(iota, hash, limit, binaryMode).then(result => {
+      this.ongoingDownloads--
 
+      // Process result
       if(result.isBinary) {
-        this._processBinaryChunk(result.data)
+        this._processBinaryChunk(result.data, batchId)
       } else {
         const signatures = result.data.filter(notNull)
         if(signatures && signatures.length === limit) {
-          this.chunkBuffer = this.chunkBuffer.concat(signatures)
+          this.batches[batchId] = signatures
         } else {
           this.emit('error', 'Download incomplete')
         }
       }
 
+      // Check if finished
+      if (this.numChunks - this.chunkOffset <= 0 && this.ongoingDownloads === 0) {
+        this.isDownloadFinished = true
+      }
+
       this._pushChunk()
     }).catch(error => {
+      this.ongoingDownloads--
       this.emit('error', error)
     })
   }
-  _processBinaryChunk (buffer) {
+  _processBinaryChunk (buffer, batchId) {
+    const batch = []
     const bytes = new Uint8Array(buffer)
     const maxOffset = bytes.length - 2
 
@@ -99,7 +128,9 @@ export default class DownloadStream extends Readable {
       length = (bytes[offset] << 8) | bytes[offset+1]
       chunk = new Uint8Array(buffer, offset + 2, length)
       offset += 2 + length
-      this.chunkBuffer.push(chunk)
+      batch.push(chunk)
     }
+
+    this.batches[batchId] = batch
   }
 }
