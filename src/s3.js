f72fa8810d0eddb50ebfe8d083928602838212d5
Edmund Mai <edmundmai@gmail.com>
2018-09-24 13:47:13 -0400

    fix bug with creating s3 with updated config

--- src/s3.js
+++ src/s3.js
@@ -1,8 +1,11 @@
 import AWS from "aws-sdk";
 
-const s3 = new AWS.S3();
+const upload = (...args) => {
+  const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+  return s3.upload(...args);
+};
 
 export default {
   config: AWS.config,
-  upload: s3.upload
+  upload
 };
