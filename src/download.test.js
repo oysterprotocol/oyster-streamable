f164cf55ad27c5a24c98eb9622a606d6b331c06d
Edmund Mai <edmundmai@gmail.com>
2018-10-16 18:51:34 -0400

    mock for download.oysternodes.com too -- still have cross origin null issue

--- src/download.test.js
+++ src/download.test.js
@@ -19,6 +19,14 @@ test("Download.toBuffer emits the expected events", done => {
     .post("/")
     .reply(200, mockIotaResponse);
 
+  nock("https://download.oysternodes.com:14265")
+    .options("/", { headers: { origin: "blah" } })
+    .reply(200, {});
+
+  nock("https://download.oysternodes.com:14265")
+    .post("/")
+    .reply(200, mockIotaResponse);
+
   const d = Download.toBuffer(TEST_HANDLE, {
     iotaProviders: [
       { provider: "https://poll.oysternodes.com:14265/" },
@@ -41,3 +49,39 @@ test("Download.toBuffer emits the expected events", done => {
 
   d.on(EVENTS.FINISH, () => done());
 });
+
+// test("Download.toBlob emits the expected events", done => {
+// const mockIotaResponse = {
+// data: { ixi: { signatures: { data: ["something"] } } }, // TODO: replace "something"
+// };
+
+// nock("https://poll.oysternodes.com:14265")
+// .options("/")
+// .reply(200, {});
+
+// nock("https://poll.oysternodes.com:14265")
+// .post("/")
+// .reply(200, mockIotaResponse);
+
+// const d = Download.toBlog(TEST_HANDLE, {
+// iotaProviders: [
+// { provider: "https://poll.oysternodes.com:14265/" },
+// { provider: "https://download.oysternodes.com:14265/" },
+// ],
+// });
+// expect.assertions(2);
+
+// d.on(EVENTS.METADATA, metadata => {
+// expect(metadata).toEqual({
+// ext: "rtf",
+// fileName: "test-file.rtf",
+// numberOfChunks: 1,
+// });
+// });
+
+// d.on(EVENTS.DOWNLOAD_PROGRESS, ({ progress }) => {
+// expect(progress).toEqual(100);
+// });
+
+// d.on(EVENTS.FINISH, () => done());
+// });
