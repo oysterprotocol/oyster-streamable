dfb5092d109671b1a58b2cd2ad9b62b359e211d8
Edmund Mai <edmundmai@gmail.com>
2018-10-28 16:17:05 +0700

    adds tests for file processor

---
+++ src/utils/file-processor.test.js
@@ -0,0 +1,12 @@
+import * as fileProcessor from "./file-processor.js";
+
+test("createMetaData", () => {
+  const fileName = "foobar.txt";
+  const numberOfChunks = 10;
+
+  expect(fileProcessor.createMetaData(fileName, numberOfChunks)).toEqual({
+    fileName: "foobar.txt",
+    ext: "txt",
+    numberOfChunks
+  });
+});
